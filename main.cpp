7bed93648cbcfd8cab01d507b2a34cf443349c52
DreamingRaven <geo.george@live.co.uk>
2017-12-11 15:45:51 +0000

    changed main.cpp include mlpack

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <armadillo>
-#include <mlpack> // please make sure path is set properly in CMakeLists.txt
+#include <mlpack/core.hpp> // please make sure path is set properly in CMakeLists.txt
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
