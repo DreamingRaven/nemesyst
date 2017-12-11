8487ccc8bc8cd200d5a18d417939247d6df9479d
DreamingRaven <geo.george@live.co.uk>
2017-12-11 23:44:32 +0000

    housekeeping

--- main.cpp
+++ main.cpp
@@ -2,7 +2,12 @@
 #include <armadillo>
 #include <mlpack/core.hpp> // please make sure path is set properly in CMakeLists.txt
 
-int main() {
+int main(int argc, char** argv) {
+
+    // process command line arguments
+
+
+    // test output
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
