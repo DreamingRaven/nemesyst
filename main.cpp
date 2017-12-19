27dd14f3687e2e1c0b64c0266b23590fba9a351b
DreamingRaven <geo.george@live.co.uk>
2017-12-19 16:49:26 +0000

    added barebones CLI arg system

--- main.cpp
+++ main.cpp
@@ -1,17 +1,55 @@
+#ifndef MAIN_ORS
+#define MAIN_ORS
+
 #include <iostream>
-#include <armadillo>
 #include <mlpack/core.hpp> // please make sure path is set properly in CMakeLists.txt
+//#include <armadillo>
 
-int main(int argc, char** argv) {
+// prototype misc functions
+void helpScreen();
 
+int main(int argc, char* argv[]) {
+
+    int fileArgPos = 0;
     // process command line arguments
+    try
+    {
+        // cycle through arguments
+        for(int i = 0; i < argc; ++i)
+        {
+            std::string arg(argv[i]);
+            if(i == 0)                                  {;} // do nothing
+            else if(arg == "-h" || arg == "--help"  )   {helpScreen();}
+            else if(arg == "-f" || arg == "--file"  )   {if(!fileArgPos){fileArgPos = i + 1;++i;std::cout << "File: " << argv[fileArgPos]<< std::endl;}}
+            else                                        {std::cout << arg << " - not flag"<< std::endl;            }
+        }
+    }
+    catch(std::exception &e)
+    {
+
+    }
 
     // defining data object
     arma::mat data;
     // filling data object
-    mlpack::data::Load("../DataSets/ml-20m/ratings.csv", data, true);
+    //mlpack::data::Load("../DataSets/ml-20m/ratings.csv", data, true);
+    if(fileArgPos)
+    {
+
+        //mlpack::data::Load( const filePath, data, true);
+    }
+
+
 
     // test output
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+
+void helpScreen()
+{
+    std::cout << "OpenRecSyst:Help"
+              << std::endl;
+}
+
+#endif
