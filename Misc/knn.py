df929ceb09e68d03ddac2de1f6d560199fa58839
DreamingRaven <geo.george@live.co.uk>
2018-01-04 17:14:36 +0000

    knn and bash args

---
+++ Misc/knn.py
@@ -0,0 +1,34 @@
+#!usr/bin/env python3.6
+import os
+import sys
+from sklearn.neighbors import NearestNeighbors
+import numpy as np
+
+
+# creating prepend variable for logging
+prePend = "[ " + os.path.basename(sys.argv[0]) + " ] "
+
+# outputting debug info
+cwd = os.getcwd()
+print(prePend, "Current wd: ", cwd)
+print(prePend, "Args: ", str(sys.argv))
+
+# first arg
+# setting data folder path with possible args(a if condition else b)
+dataFolderPath = "../../../DataSets/ml-20m/"  # this is the default path
+dataFolderPath = dataFolderPath if len(sys.argv) == 1 else sys.argv[1]
+print(prePend, "Data path: ", dataFolderPath)
+
+# second arg
+# setting data file name
+testFileName = "test.csv"  # default value
+testFileName = sys.argv[2] if len(sys.argv) >= 3 else testFileName
+print(prePend, "Test file name: ", testFileName)
+
+# third arg
+# setting data file name
+trainFileName = "train.csv"  # default value
+trainFileName = sys.argv[3] if len(sys.argv) >= 4 else trainFileName
+print(prePend, "Train test %: ", trainFileName)
+
+print(prePend, "Fin.")
