8ba06e8e2792f5d1be45b06c16877f22e1c4f148
DreamingRaven <geo.george@live.co.uk>
2018-01-19 01:33:10 +0000

    implemented joinable data

--- Misc/movieLenseDataCombiner.py
+++ Misc/movieLenseDataCombiner.py
@@ -21,7 +21,7 @@ dataFolderPath = dataFolderPath if len(sys.argv) == 1 else sys.argv[1]
 print(prePend, "Data path:", dataFolderPath)
 
 # check if data exists
-if os.path.isfile(dataFolderPath + "pML.csv") and 1 == 0:
+if os.path.isfile(dataFolderPath + "pML.csv"):# and 1 == 0:
     print(prePend, "pML.csv found... Skipping.")
 else: # else generate csv
     print(prePend, "pML.csv not found... Generating.")
@@ -48,20 +48,34 @@ else: # else generate csv
     df = pd.DataFrame(columns=fullFeatureList)
     print(prePend, df)
 
-    reformedMetaData = pd.DataFrame() # create the empty metadata half
-
     i = 0 # not important here so you can see progress
+    x = [] # list of lists
+    specificColumnNames = [] # cry me a river
     for name, group in eachMoviesTags:
         i += 1
         # group is of type pandas DataFrame meaning concat will be easier
         groupTemp = group.transpose() # transpose to be tag column major order
         groupTemp.columns = groupTemp.iloc[2] # make a specific row the columns
         groupTemp = groupTemp.drop(['tag', 'movieId'], 0) # remove some rows
-        groupTemp = groupTemp.reset_index(drop=True) # get rid of previous index
+        #groupTemp = groupTemp.reset_index(drop=True) # get rid of previous index
+        #groupTemp = groupTemp.rename_axis(None)
         groupTemp['movieId'] = name # add id column
-        #if (i % 10) == 0:
-        print(prePend, "progress:", i, "/", len(eachMoviesTags), " movieId: ", groupTemp['movieId'][0])
+        if (i % 10) == 0:
+            print(prePend, "progress:", i, "/", len(eachMoviesTags), " movieId: ", groupTemp['movieId'][0])
+            if i == 10:
+                specificColumnNames = groupTemp.columns.tolist()
+        x.append(groupTemp.values.tolist())
 
+
+    #print(x)
+    print(prePend, specificColumnNames)
+    #reformedMetaData = pd.DataFrame(x.values.tolist(), columns=['lists'])#, columns=specificColumnNames)
+    reformedMetaData = pd.DataFrame(x, columns=['lists'])#, columns=specificColumnNames)
+    reformedMetaData = pd.DataFrame(reformedMetaData['lists'].values.tolist(), columns=specificColumnNames)
+
+    #reformedMetaData.columns = specificColumnNames
+    #reformedMetaData = pd.concat(x)
+    print(prePend, reformedMetaData)
     # df.to_csv((dataFolderPath + "zFullFeatureFile.csv"), encoding='utf-8', index=False)
 
     #uniqueTags = uniqueTags.tolist()
@@ -73,6 +87,7 @@ else: # else generate csv
     pML = ratings
 
     # outputting data
-    #pML.to_csv( (dataFolderPath + "pML.csv"), encoding='utf-8', index=False)
+    pML.to_csv( (dataFolderPath + "pML.csv"), encoding='utf-8', index=False)
+    reformedMetaData.to_csv(dataFolderPath + "pMLMetaReformed.csv", encoding='utf-8', index=False)
 
 print(prePend, "Fin.")
