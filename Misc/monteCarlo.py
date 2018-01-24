df61ff68daa8e43dd29a1be7e8801c6a915e0702
DreamingRaven <geo.george@live.co.uk>
2018-01-24 12:44:06 +0000

    added more modularisation to monteCarlo

--- Misc/monteCarlo.py
+++ Misc/monteCarlo.py
@@ -16,21 +16,33 @@ print(prePend, "Args: ", str(sys.argv))
 
 # first arg
 # setting data folder path with possible args(a if condition else b)
-dataFolderPath = "../../../DataSets/ml-20m/" # this is the default path
+dataFolderPath = "../../../DataSets/ml-20m/"  # this is the default path
 dataFolderPath = dataFolderPath if len(sys.argv) == 1 else sys.argv[1]
 print(prePend, "Data path: ", dataFolderPath)
 
 # second arg
 # setting data file name
-dataFileName = "pML.csv" # default value
+dataFileName = "pML.csv"  # default value
 dataFileName = sys.argv[2] if len(sys.argv) >= 3 else dataFileName
 print(prePend, "Data file name: ", dataFileName)
 
 # third arg
 # setting data file name
-testSize = 0.2 # default value
+testSize = 0.2  # default value
 testSize = float(sys.argv[3]) if len(sys.argv) >= 4 else testSize
 print(prePend, "Split test %: ", testSize)
+
+# fourth arg
+# setting data file name
+testFileName = "test.csv"  # default value
+testFileName = sys.argv[4] if len(sys.argv) >= 5 else testFileName
+print(prePend, "Data file name: ", testFileName)
+
+# fifth arg
+# setting data file name
+trainFileName = "train.csv"  # default value
+trainFileName = sys.argv[5] if len(sys.argv) >= 6 else trainFileName
+print(prePend, "Data file name: ", trainFileName)
 
 # check if data exists
 if (os.path.isfile(dataFolderPath + "train.csv") and
