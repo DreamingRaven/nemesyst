b87ed9a3d0fb3fbf1fd1f6919ae6d3a6eb4137bf
DreamingRaven <geo.george@live.co.uk>
2018-01-30 16:04:12 +0000

    completed knnRegression

--- Misc/knn.py
+++ Misc/knn.py
@@ -6,11 +6,12 @@ import os
 import sys
 import numpy as np
 import pandas as pd
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, mean_absolute_error
 from sklearn.neighbors import NearestNeighbors
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.model_selection import train_test_split
+from sklearn.externals import joblib  # lib to allow model presistence
 
 # creating prepend variable for logging
 prePend = "[ " + os.path.basename(sys.argv[0]) + " ] "
@@ -54,30 +55,28 @@ print(prePend, "Target name: ", targetName)
 test = pd.read_csv(dataFolderPath + testFileName)  # not used for parameters
 train = pd.read_csv(dataFolderPath + trainFileName)  # further split
 
-# remove target in test # tbh its not necessary to store this
-#test_noLabel = test[test.columns.difference(['rating'])]
-
 # subsetting training set for validation preventing overfitting on real test
 trainTrain, trainTest = train_test_split(train, test_size=testSize)
 
-# debug
-#print(test_noLabel.head())
-
 # instantiate model
-#knn = KNeighborsClassifier(n_neighbors=3)
 knn = KNeighborsRegressor(n_neighbors=3)
 
 # train model
-#nnModel = NearestNeighbors(n_neighbors=5, algorithm="ball_tree").fit(train)
-# takes a tuple of(X, y) X = training data y = target vals
 knn.fit( trainTrain[ trainTrain.columns.difference([targetName]) ],
          trainTrain[targetName])
 
 # find distances to test set / make predictions
-#distances, indices = nnModel.kneighbors(test_noLabel)
-#pred = knn.predict(test_noLabel)
-pred = knn.predict( trainTest[trainTrain.difference([targetName])] )
+pred = knn.predict( trainTest[trainTrain.columns.difference([targetName])] )
 
-#print(accuracy_score())
+# debug
+print(prePend, type(pred))
+print(prePend, pred)
+print(prePend, type(trainTest[targetName]))
+print((prePend, trainTest[targetName]))
+
+print(mean_absolute_error(trainTest[targetName], pred))
+
+# dump model onto persistent storage
+joblib.dump(knn, dataFolderPath + "/Model/knn.pkl")
 
 print(prePend, "Fin.", (time.time() - startTime), " seconds.")
