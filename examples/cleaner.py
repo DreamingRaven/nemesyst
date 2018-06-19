86b0c19e1ce13d2c2c4a8b474e96474c88c56718
DreamingRaven <geo.george@live.co.uk>
2018-06-19 15:14:34 +0000

    created example cleaner but not populated

---
+++ examples/cleaner.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# @Author: George Onoufriou <archer>
+# @Date:   2018-06-19
+# @Filename: cleaner.py
+# @Last modified by:   archer
+# @Last modified time: 2018-06-19
+# @License: Please see LICENSE file in project root
