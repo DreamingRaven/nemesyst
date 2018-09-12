6a2c0d401e0b99207c7821fbf969c721098acc99
DreamingRaven <geo.george@live.co.uk>
2018-09-12 11:10:49 +0000

    untested setup and PKGBUILD

--- setup.py
+++ setup.py
@@ -5,23 +5,27 @@
 # @Date:   2018-09-05
 # @Filename: setup.py
 # @Last modified by:   archer
-# @Last modified time: 2018-09-11
+# @Last modified time: 2018-09-12
 # @License: Please see LICENSE file in project root
 
 import subprocess as sp
-from distutils.core import setup
+from setuptools import setup
 
 
-
+# getting version from git as this is vcs
 runArgs = ["git", "describe", "--long"]
 version = sp.run(runArgs, stdout=sp.PIPE).stdout.decode("utf-8")
 
-
+with open("README.md", "r") as fh:
+    readme = fh.read()
 
 setup(
     name="nemesyst",
     version=str(version),
     description="Generalised, sequence-based, deep-learning framework of the gods. Warning may include GANs, does not include nuts.",
+    long_description=readme
+    long_description_content_type="text/markdown"
     author="George Onoufriou"
     url="https://github.com/DreamingRaven/Nemesyst"
+    packages=setuptools.find_packages()
 )
