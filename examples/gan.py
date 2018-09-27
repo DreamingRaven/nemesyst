d32483c12e96c1a7b175be3865d636c46ef4a02c
DreamingRaven <GeorgeRavenNR@outlook.com>
2018-09-27 14:53:23 +0000

    added example-template file non-pop yet

---
+++ examples/gan.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# @Author: George Onoufriou <archer>
+# @Date:   2018-09-27
+# @Filename: gan.py
+# @Last modified by:   archer
+# @Last modified time: 2018-09-27
+# @License: Please see LICENSE file in project root
+
+import sys
+
+from src.arg import argz
+description="Nemesyst template file"
+args = argz(sys.argv[1:], description=description)
+
+print(sys.path)
