50dd52d4a8519b90e5c4282334e6fe72ea7562b8
DreamingRaven <GeorgeRavenNR@outlook.com>
2018-12-03 12:09:26 +0000

    updated gan to use class structure now to implement

--- examples/gan.py
+++ examples/gan.py
@@ -3,8 +3,8 @@
 # @Author: George Onoufriou <archer>
 # @Date:   2018-09-27
 # @Filename: gan.py
-# @Last modified by:   georgeraven
-# @Last modified time: 2018-12-02
+# @Last modified by:   archer
+# @Last modified time: 2018-12-03
 # @License: Please see LICENSE file in project root
 
 import sys
@@ -16,31 +16,40 @@ prePend = "[ " + fileName + " ] "
 # this is calling system wide nemesyst src.arg so if you are working on a branch
 # dont forget this will be the main branch version of args
 
+
 def main(args, db, log):
 
-    log( prePend + "\n\tArg dict of length: " + str(len(args)) +
+    log(prePend + "\n\tArg dict of length: " + str(len(args)) +
         "\n\tDatabase obj: " + str(db) + "\n\tLogger object: " + str(log), 0)
 
+    gan = GAN()
+
     if(args["toTrain"]):
-        train(args=args, db=db, log=log)
+        gan.train()
 
     if(args["toTest"]):
-        test(args=args, db=db, log=log)
+        gan.test()
 
     if(args["toPredict"]):
-        predict(args=args, db=db, log=log)
+        gan.predict()
 
 
-def train(args, db, log):
-    None
+class GAN():
 
+    def __init__(self):
+        None
 
+    def train(self):
+        # if training new model
+        # else continuing training
+        None
 
+    def test(self):
+        # if testing using previously trained model
+        # else testing using just-trained model (as in model already in memory)
+        None
 
-def test(args, db, log):
-    None
-
-
-
-def predict(args, db, log):
-    None
+    def predict(self):
+        # if testing using previously trained model
+        # else testing using just-trained model (as in model already in memory)
+        None
