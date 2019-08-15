225a5ffeb1fe55bac24a2fd8c913f10d0178b291
DreamingRaven <GeorgeRavenNR@outlook.com>
2019-08-16 00:35:40 +0100

    Added new debug learner and updated header

--- scripts/learners/debug_learner.py
+++ scripts/learners/debug_learner.py
@@ -1,7 +1,7 @@
 # @Author: George Onoufriou <archer>
-# @Date:   2019-08-15
+# @Date:   2019-08-16
 # @Email:  george raven community at pm dot me
-# @Filename: debug_cleaner.py
+# @Filename: debug_learner.py
 # @Last modified by:   archer
 # @Last modified time: 2019-08-16
 # @License: Please see LICENSE in project root
