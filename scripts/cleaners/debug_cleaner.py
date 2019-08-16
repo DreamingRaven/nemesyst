0fa4173cbd69373ca859292d687f305e1d53d02c
DreamingRaven <GeorgeRavenNR@outlook.com>
2019-08-16 17:07:22 +0100

    Added db into debug scripts

--- scripts/cleaners/debug_cleaner.py
+++ scripts/cleaners/debug_cleaner.py
@@ -3,10 +3,10 @@
 # @Email:  george raven community at pm dot me
 # @Filename: debug_cleaner.py
 # @Last modified by:   archer
-# @Last modified time: 2019-08-16T02:03:34+01:00
+# @Last modified time: 2019-08-16
 # @License: Please see LICENSE in project root
 
 
 def main(**kwargs):
     print("kwargs:", type(kwargs), kwargs)
-    yield {}
+    # yield {}
