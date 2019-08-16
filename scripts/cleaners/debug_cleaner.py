7818d63277c93a983ea15f5f996217e2585f00e3
DreamingRaven <GeorgeRavenNR@outlook.com>
2019-08-16 02:06:51 +0100

    Added generator to debug funcs

--- scripts/cleaners/debug_cleaner.py
+++ scripts/cleaners/debug_cleaner.py
@@ -3,9 +3,10 @@
 # @Email:  george raven community at pm dot me
 # @Filename: debug_cleaner.py
 # @Last modified by:   archer
-# @Last modified time: 2019-08-16
+# @Last modified time: 2019-08-16T02:03:34+01:00
 # @License: Please see LICENSE in project root
 
 
-def main(*args):
-    print("args:", type(args), args)
+def main(**kwargs):
+    print("kwargs:", type(kwargs), kwargs)
+    yield {}
