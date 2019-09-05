a38fc83679201f29c4263dacc1fa89fd0637913f
DreamingRaven <GeorgeRavenNR@outlook.com>
2019-09-05 14:11:55 +0100

    Added commented out logo
    this will later be enabled when the logo is up to snuff

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -89,6 +89,10 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"  # 'alabaster'
+# html_logo = "logo.svg"
+# html_theme_options = {
+#     "logo_only": False,
+# }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
