8e064ed793803bc3e3fa476bf9f445c37b04130b
DreamingRaven <GeorgeRavenNR@outlook.com>
2019-10-01 22:11:42 +0100

    Updating mnist cleaner to download mnist
    downloading mnist is a little more complex than i gave it credit as its
    compressed and contains multiple files soooooo awkward to work with
    unless we actually just dump it on the file system.

--- examples/cleaners/mnist_cleaner.py
+++ examples/cleaners/mnist_cleaner.py
@@ -5,10 +5,29 @@
 # @Last modified by:   archer
 # @Last modified time: 2019-08-16
 # @License: Please see LICENSE in project root
+import urllib.request
+import gzip
+import io
+
+
+def download(url):
+    r = urllib.request.urlopen(url)
+    return r
 
 
 def main(**kwargs):
     print("kwargs:", type(kwargs), kwargs)
+
+    print("downloading mnist dataset...")
+
+    r = download('http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz')
+
+    print(r)
+    compressed_file = io.BytesIO(r.read())
+    decompressed_file = gzip.GzipFile(fileobj=compressed_file)
+    with open("test_gzip", 'wb') as outfile:
+        outfile.write(decompressed_file.read())
+
     x = 0
     while x < 10:
         yield {"x": x}
