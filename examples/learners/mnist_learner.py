fefe6299adc06ddeb3779e36833b3df1d919ad9e
DreamingRaven <GeorgeRavenNR@outlook.com>
2019-10-09 13:41:58 +0100

    Added mnist example comparing to keras
    comparing keras inbuilt example to our mnist to make sure they are
    compatible.

--- examples/learners/mnist_learner.py
+++ examples/learners/mnist_learner.py
@@ -28,4 +28,41 @@ def main(**kwargs):
         x = np.array(x)  # converting list of lists to numpy ndarray
     args["pylog"](x, y)
     args["pylog"](x.shape, y.shape)
+
+    # args["pylog"](x.reshape(x.shape[0], 28, 28, 1).shape())
+
+    # testing keras inbuilt method
+    from keras.datasets import mnist
+    from keras.models import Sequential
+    from keras.layers import Dense, Dropout, Flatten
+    from keras.layers import Conv2D, MaxPooling2D
+    from keras import backend as K
+
+    batch_size = 128
+    num_classes = 10
+    epochs = 12
+
+    # input image dimensions
+    img_rows, img_cols = 28, 28
+
+    # the data, split between train and test sets
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    if K.image_data_format() == 'channels_first':
+        x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+        x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+        input_shape = (1, img_rows, img_cols)
+    else:
+        x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+        x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+        input_shape = (img_rows, img_cols, 1)
+
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+    x_train /= 255
+    x_test /= 255
+    print('x_train shape:', x_train.shape)
+    print(x_train.shape[0], 'train samples')
+    print(x_test.shape[0], 'test samples')
+
     yield {}
