476c4a8b6bd020c08318f990655f20ec44a6194e
DreamingRaven <GeorgeRavenNR@outlook.com>
2019-12-31 17:21:53 +0000

    Updated all calls to argument_parser
    Now that the argument handler has been moved ensuring both docs and
    nemesyst still build and function properly

--- examples/learners/mnist_learner.py
+++ examples/learners/mnist_learner.py
@@ -3,7 +3,7 @@
 # @Email:  george raven community at pm dot me
 # @Filename: mnist_learner.py
 # @Last modified by:   archer
-# @Last modified time: 2019-10-17T00:36:20+01:00
+# @Last modified time: 2019-12-31T17:18:10+00:00
 # @License: Please see LICENSE in project root
 
 import numpy as np
@@ -17,7 +17,7 @@ from keras.layers import Conv2D, MaxPooling2D
 
 
 def main(**kwargs):
-    """Main entry point called by Nemesyst, always yields dictionary or None.
+    """Entry point called by Nemesyst, always yields dictionary or None.
 
     :param **kwargs: Generic input method to handle infinite dict-args.
     :rtype: yield dict
@@ -25,8 +25,8 @@ def main(**kwargs):
     # # there are issues using RTX cards with tensorflow:
     # # https://github.com/tensorflow/tensorflow/issues/24496
     # # if this is the case please uncomment the following two lines:
-    # import os
-    # os.environ['CUDA_VISIBLE_DEVICES'] = '-1' # use cpu
+    import os
+    os.environ['CUDA_VISIBLE_DEVICES'] = '-1'  # use cpu
 
     # just making these a little nicer to read but in a real application
     # we would not want these hardcoded thankfully the database can provide!
